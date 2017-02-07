b849d273952315cdbe1380ecb1b2bcf83eec3a54
INDUJA N <indhu.hari22@gmail.com>
2017-02-07 16:26:28 +0530

    third commit

--- src/com/example/grpahics/MainActivity.java
+++ src/com/example/grpahics/MainActivity.java
@@ -1,142 +1,109 @@
-/*
 package com.example.grpahics;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	
+	EditText ed,ed1,ed2,ed3;
+	Button r,c,s,t;
+	String sh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}
-*/
-
-
-
-
-
-package com.example.grpahics;  
-
-import android.support.v7.app.ActionBarActivity;  
-import android.os.Bundle;  
-import android.app.Activity;  
-import android.view.Menu;  
-import android.content.Context;  
-import android.graphics.Canvas;  
-import android.graphics.Color;  
-import android.graphics.Paint;  
-import android.view.View;  
-import android.widget.Button;
-  
-public class MainActivity extends ActionBarActivity {  
-  
-	Button line,Arc,Rectangle,Square,Circle,Oval;
-     
-   
-    /** Called when the activity is first created. */  
-    @Override  
-    public void onCreate(Bundle savedInstanceState) {  
-        super.onCreate(savedInstanceState);    
-        setContentView(R.layout.activity_main);
-        line=(Button) findViewById(R.id.li);
-        Arc=(Button) findViewById(R.id.ar);
-        Rectangle=(Button) findViewById(R.id.re);
-        Square=(Button) findViewById(R.id.sq);
-        Circle=(Button) findViewById(R.id.ci);
-        Oval=(Button) findViewById(R.id.ov);
         
-        line.setOnClickListener(new View.OnClickListener() 
-        {
-        			
-			protected void onDraw(Canvas canvas)   
-			{ 
-				
-				Paint line = new Paint();
-				line.setStyle(Paint.Style.FILL);
-				
-			            
-					  
-			            // custom drawing code here  
-			            Paint paint = new Paint();  
-			            paint.setStyle(Paint.Style.FILL);  
-			  
-			            // make the entire canvas white  
-			            paint.setColor(Color.WHITE);  
-			            canvas.drawPaint(paint);  
-				// TODO Auto-generated method stub
-				
-			}
+        ed=(EditText) findViewById(R.id.ed);
+        ed1=(EditText) findViewById(R.id.ed2);
+        ed2=(EditText) findViewById(R.id.l);
+        ed3=(EditText) findViewById(R.id.b);
+        r=(Button) findViewById(R.id.b1);
+        c=(Button) findViewById(R.id.b2);
+        s=(Button) findViewById(R.id.b3);
+        t=(Button) findViewById(R.id.b4);
+        r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	sh="RECTANGLE";
+            	String data1=ed.getText().toString();
+            	String data2=ed1.getText().toString();
+            	String data3=ed2.getText().toString();
+            	String data4=ed3.getText().toString();
+            	
+            
+            	Intent i=new Intent(MainActivity.this,sec_Activity.class);
+				i.putExtra("username", sh);
+				i.putExtra("userdata1", data1);
+				i.putExtra("userdata2", data2);
+				i.putExtra("userdata3", data3);
+				i.putExtra("userdata4", data4);
+				startActivity(i);
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	sh="CIRCLE";
+            	String data1=ed.getText().toString();
+            	String data2=ed1.getText().toString();
+            	String data3=ed2.getText().toString();
+            	String data4=ed3.getText().toString();
+            	Intent i=new Intent(MainActivity.this,sec_Activity.class);
+				i.putExtra("username", sh);
+				i.putExtra("userdata1", data1);
+				i.putExtra("userdata2", data2);
+				i.putExtra("userdata3", data3);
+				i.putExtra("userdata4", data4);
+				startActivity(i);
+            }
+        });
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	sh="SQUARE";
+            	String data1=ed.getText().toString();
+            	String data2=ed1.getText().toString();
+            	String data3=ed2.getText().toString();
+            	String data4=ed3.getText().toString();
+            	Intent i=new Intent(MainActivity.this,sec_Activity.class);
+				i.putExtra("username", sh);
+				i.putExtra("userdata1", data1);
+				i.putExtra("userdata2", data2);
+				i.putExtra("userdata3", data3);
+				i.putExtra("userdata4", data4);
+				startActivity(i);
+            }
+        });
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	sh="TRIANGLE";
+            	String data1=ed.getText().toString();
+            	String data2=ed1.getText().toString();
+            	String data3=ed2.getText().toString();
+            	String data4=ed3.getText().toString();
+            	Intent i=new Intent(MainActivity.this,sec_Activity.class);
+				i.putExtra("username", sh);
+				i.putExtra("userdata1", data1);
+				i.putExtra("userdata2", data2);
+				i.putExtra("userdata3", data3);
+				i.putExtra("userdata4", data4);
+				startActivity(i);
+            }
+        });
+    }
 
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-    }  
+   
+    
 }
-/*
-        @Override protected void onDraw(Canvas canvas) {  
-            
-  
-            // custom drawing code here  
-            Paint paint = new Paint();  
-            paint.setStyle(Paint.Style.FILL);  
-  
-            // make the entire canvas white  
-            paint.setColor(Color.WHITE);  
-            canvas.drawPaint(paint);  
-            
-            // draw blue circle with anti aliasing turned off  
-            paint.setAntiAlias(false);  
-            paint.setColor(Color.BLUE);  
-            canvas.drawCircle(20, 20, 15, paint);  
-  
-            // draw green circle with anti aliasing turned on  
-            paint.setAntiAlias(true);  
-            paint.setColor(Color.GREEN);  
-            canvas.drawCircle(60, 20, 15, paint);  
-  
-            // draw red rectangle with anti aliasing turned off  
-            
-            paint.setAntiAlias(false);  
-            paint.setColor(Color.RED);  
-            canvas.drawRect(100, 5, 200, 30, paint);  
-                                         
-            //undo the rotate  
-            canvas.restore();  
-        }  
-    }  
-    @Override  
-    public boolean onCreateOptionsMenu(Menu menu) {  
-        // Inflate the menu; this adds items to the action bar if it is present.  
-        getMenuInflater().inflate(R.menu.main, menu);  
-        return true;  
-    }  
-}  
-     */
---
+++ src/com/example/grpahics/sec_Activity.java
@@ -0,0 +1,81 @@
+package com.example.grpahics;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+public class sec_Activity extends Activity{
+	ShapeView shapeview;
+	String data,data1,data2,data3,data4;
+	int x,y,l,b;
+	protected void onCreate(Bundle  savedInstanceState)
+	{
+	super.onCreate(savedInstanceState);
+	shapeview=new ShapeView(this);
+	setContentView(shapeview);
+	Intent i=getIntent();
+	data= i.getStringExtra("username");
+	data1=i.getStringExtra("userdata1");
+	data2=i.getStringExtra("userdata2");
+	data3=i.getStringExtra("userdata3");
+	data4=i.getStringExtra("userdata4");
+	x=Integer.parseInt(data1);
+	y=Integer.parseInt(data2);
+	l=Integer.parseInt(data3);
+	b=Integer.parseInt(data4);
+	}
+	public class ShapeView extends View
+	{
+
+		public ShapeView(Context context) {
+			super(context);
+			// TODO Auto-generated constructor stub
+		}
+		
+		
+		protected void onDraw(Canvas canvas)
+		{
+		super.onDraw(canvas);
+		 
+		// custom drawing code here
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+
+		// make the entire canvas white
+		paint.setColor(Color.BLACK);
+		canvas.drawPaint(paint);
+
+		
+		// draw green circle with anti aliasing turned on
+		if(data.equals("circle"))
+		{
+		paint.setAntiAlias(true);
+		paint.setColor(Color.BLUE);
+		canvas.drawCircle(x, y, l, paint);
+		}
+
+		// draw red rectangle with anti aliasing turned off
+		if(data.equals("rectangle"))
+		{
+		paint.setAntiAlias(false);
+		paint.setColor(Color.MAGENTA);
+		canvas.drawRect(x, y, l, b, paint);
+		}
+		if(data.equals("square"))
+		{
+		paint.setAntiAlias(false);
+		paint.setColor(Color.MAGENTA);
+		canvas.drawRect(x, y, l, b, paint);
+		}
+
+	
+		
+	}	
+	}
+	
+	
+}
